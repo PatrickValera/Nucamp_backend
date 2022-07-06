220ac416a7e0afae47ee2e984fc55d80b9c696a6
PatrickMac <patrickvalera50@yahoo.com>
2022-07-06 08:30:24 -0700

    week 2

---
+++ Python/1-Fundamentals/week2.py
@@ -0,0 +1,31 @@
+stars = ""
+for i in range(0, 5, 1):
+    for j in range(0, i, 1):
+        stars = stars + "*"
+    print(stars)
+# FOR LOOP LOADING BAR======
+# for amount_loaded in range(0, 101, 5):
+#     print(amount_loaded)
+#     if amount_loaded == 25:
+#         print("1/4 of the way there")
+#     elif amount_loaded == 50:
+#         print("1/2 of the way there")
+#     elif amount_loaded == 75:
+#         print("3/4 of the way there")
+#     elif amount_loaded == 100:
+#         print("Loading complete!")
+
+# FOR LOOP 2 ==============
+# for x in range(0, 7, 1):
+#     if x == 0 or x == 6:
+#         print("*")
+#     elif x == 1 or x == 5:
+#         print("**")
+#     elif x == 2 or x == 3:
+#         print("**")
+#     else:
+#         print("****")
+
+# FOR LOOP 1 ==============
+# for i in range(20, -21, -5):
+#     print(i)
