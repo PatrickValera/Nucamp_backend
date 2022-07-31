98e2ba9e01397d6c6142fcc92779ee158fd7e5f4
PatrickMac <patrickvalera50@yahoo.com>
2022-07-31 03:17:29 -0700

    w5

--- Python/1-Fundamentals/week5/week5.py
+++ Python/1-Fundamentals/week5/week5.py
@@ -43,11 +43,62 @@ linear_search_dictionary(my_dictionary, 5)
 linear_search_dictionary(my_dictionary, 3)
 linear_search_dictionary(my_dictionary, 8)'''
 
-# Excercise: Bubble Sort
-unsorted_list = [101,49,3,12,56]
+# Excercise: Bubble Sort =======
+'''unsorted_list = [101,49,3,12,56]
 
 def bubblesort(the_list):
     high_idx=len(the_list)-1
     
-    # for i in range(high_idx):
-    #     for j in 
+    for i in range(high_idx):
+        list_changed=False
+        for j in range(high_idx):
+            item = the_list[j]
+            next = the_list[j+1]
+            if item>next:
+                the_list[j]=next
+                the_list[j+1]=item
+                list_changed=True
+        # If there was no swapping in a pass-thru then it's sorted, so break
+        if list_changed:break
+
+bubblesort(unsorted_list)
+print(unsorted_list)'''
+
+
+# Excercise: Quicksort
+# Divide and Conquer. A pivot is chosen first
+
+'''unsorted_list = [101,49,3,12,56]
+
+def sort_part(the_list,low_idx,high_idx):
+    pivot_idx=high_idx
+
+    while low_idx!=pivot_idx:
+        low_val=the_list[low_idx]
+        if low_val>the_list[pivot_idx]:
+            the_list[low_idx]=the_list[pivot_idx-1]
+            the_list[pivot_idx-1]=the_list[pivot_idx]
+            the_list[pivot_idx]=low_val
+            pivot_idx-=1
+        else:
+            low_idx+=1
+    return pivot_idx
+
+def quicksort(the_list,low_idx,high_idx):
+    if low_idx>high_idx: return
+    pivot_idx=sort_part(the_list,low_idx,high_idx)
+    quicksort(the_list,low_idx,pivot_idx-1)
+    quicksort(the_list,pivot_idx+1,high_idx)
+
+quicksort(unsorted_list,0,len(unsorted_list)-1)
+print(unsorted_list)'''
+
+# Code Challenge: FizzBuzz
+def fizzbuzz(num):
+    for i in range(1,num+1):
+        if i%3 == 0 and i%5==0: print("FizzBuzz")
+        elif i%3 == 0: print("Fizz")
+        elif i%5 == 0: print("Buzz")
+        else:print(i)
+
+fizzbuzz(50)
